2fe82c96ebace4b7f71e645b276f3dd08d378dae
AbdelrahmanMohamed-Fathy <abdelrahman.fathy04@outlook.com>
2025-03-17 14:54:37 +0200

    Co-authored-by: Ahmed Mohamed Farouk <a7medfarouk@users.noreply.github.com> Co-authored-by: Mohamed Raed <Raed-04@users.noreply.github.com>

---
+++ frontend/components/loginScreen.tsx
@@ -0,0 +1,13 @@
+import { useNavigation } from "@react-navigation/native"
+
+export default function LoginScreen() {
+    const navigate = useNavigation();
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    return (
+        <View>
+            
+        </View>
+    )
+}
