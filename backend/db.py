9b14c798c889049012d746538c40ea196e363a2c
MoAlkhateeb <mo.hany.alkhateeb@gmail.com>
2025-04-28 16:24:38 +0300

    Added postgress support

--- backend/db.py
+++ backend/db.py
@@ -1,8 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-DATABASE_URL = "sqlite:///./db.sqlite"
+load_dotenv()
+
+DATABASE_URL = os.environ.get("DB_CONNECTION")
 
 engine = create_engine(DATABASE_URL, echo=True)
 
